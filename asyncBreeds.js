39f1d72306bfea19881e6d2b4cd1d119b21ee1ba
Jack Kang <jkang0724@hotmail.com>
2022-11-27 21:50:08 -0500

    Create assertion tests for sync and async functions

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,17 +1,16 @@
 // asyncBreeds.js
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed, callback) {
+const breedDetailsFromFile = (breed, callback) => {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
-    if (!error) callback(data);
+    if (error) {
+      return callback(undefined);
+    } else callback(data);
   });
 };
 
-// CHANGE 1: Moved the console.log into a new function:
-const printOutCatBreed = breed => console.log('Return Value: ', breed);
 
-// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
-breedDetailsFromFile('Bombay', printOutCatBreed);
+module.exports = breedDetailsFromFile;
--- syncBreeds.js
+++ syncBreeds.js
@@ -5,11 +5,7 @@ const catBreeds = {
 };
 
 // synchronous function to fetch a cat breed
-const breedDetails = function(breed) {
-  // can simply return it (easy peezee, butter squeezy) ...
-  return catBreeds[breed];
-};
+const breedDetails = breed => catBreeds[breed];
 
-// get the return value right away from the function
-const bombay = breedDetails('Bombay');
-console.log(bombay); //=> prints out the description for that breed
+
+module.exports = breedDetails;
